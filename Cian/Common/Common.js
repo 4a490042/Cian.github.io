6c0eed12d9fb8d66c46edae6cb26873224b264b3
4a490042 <44599058+4a490042@users.noreply.github.com>
2021-03-19 19:17:56 +0800

    Add files via upload

---
+++ Cian/Common/Common.js
@@ -0,0 +1,12 @@
+var UI = new UIControl();
+$(document).ready(function() {
+    $('body').html(UI.GetUI("Login/view/Login.html"));
+});
+
+
+// <script type="text/javascript" src="Common/Common.js"></script>
+//     <script type="text/javascript">
+//     $.getJSON("Date/2019.json", function(data) {
+//         console.log(data);
+//     });
+//     </script>
